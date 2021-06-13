5d5264eb750e77414d2ad9a52f56fad7972df7c6
Guilherme Zaramella <guilherme@rjconsultores.com.br>
2021-06-13 15:13:26 -0300

    up project

---
+++ src/main/java/br/com/zaratech/ZaratechApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zaratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaratechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaratechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zaratech/ZaratechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zaratech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaratechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
